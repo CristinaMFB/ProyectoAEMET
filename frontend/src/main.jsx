6dc073f6e0de28d9af76ae441841e63743a670b5
CristinaMFB <3370337@murciaeduca.es>
2026-02-09 19:53:54 +0100

    Configuración de React Router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
 import './index.css'
 import App from './App.jsx'
+import { Inicio } from "./pages/Inicio.jsx";
+import { PrediccionDias } from "./pages/PrediccionDias.jsx" 
+import { PrediccionHoras } from "./pages/PrediccionHoras.jsx"
+
+const router = createBrowserRouter([ 
+  { 
+    path: "/", 
+    element: <Inicio />, 
+  }, 
+  { 
+    path: "/prediccion-dias", 
+    element: <PrediccionDias />, 
+  }, 
+  { 
+    path: "/prediccion-horas", 
+    element: <PrediccionHoras />, 
+  }, 
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
